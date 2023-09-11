a437f81443a9f760c5fac914016dbdec5a1f51cd
WilliamClarke2 <136397704+WilliamClarke2@users.noreply.github.com>
2023-09-11 00:55:03 -0400

    Update createModel.py

--- Ascribe/createModel.py
+++ Ascribe/createModel.py
@@ -2,12 +2,15 @@ import json
 import openai
 import os
 
+#loads the config file
 with open(os.getcwd() + "\\app.config") as settings_file:
    appSettings = json.load(settings_file)
 
+#defines constants based off of values from app.config
 FILE_NAME = os.getcwd() + "\\" + appSettings["tone"] + "PromptCompletions.jsonl"
 API_KEY = appSettings["api_key"]
 MODEL = appSettings["model"]
+SUFFIX = appSettings["tone"]
 openai.api_key = API_KEY
 os.environ['OPENAI_API_KEY'] = API_KEY
 
@@ -17,9 +20,12 @@ print("You have to wait a few minutes for OpenAI to process this file")
 modelCreated = False
 
 while modelCreated == False:
-	userResponse = input("Enter the word 'begin' to send a request to create a fine-tuned model")
-	if userResponse == 'begin':
-		
-		fineTune = openai.FineTuningJob.create(training_file=trainingFile.id, model = "gpt-3.5-turbo",hyperparameters={"n_epochs":1,"suffix":appSettings["tone"]})
-		print("Fine tuning process started")
-		
+    userResponse = input("Enter the word 'begin' to send a request to create a fine-tuned model: ")
+    if userResponse == 'begin':
+        try:
+            print("Fine tuning process started")
+            #1 epoch is generally enough, and is the cheapest option by far. Having multiple models with the same suffix is fine.
+            fineTune = openai.FineTuningJob.create(training_file=trainingFile.id, model = MODEL,hyperparameters={"n_epochs":1}, suffix=SUFFIX)
+			modelCreated = True
+        except:
+            print("OpenAI hasn't finished processing the file") #this takes a few minutes
